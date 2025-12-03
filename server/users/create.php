6471217128f56e818ccea3c88e9410cc67227d8a
aryprntaa <adiprants@gmail.com>
2025-12-03 23:36:23 +0700

    Menambahkan update project terakhir

--- server/users/create.php
+++ server/users/create.php
@@ -25,12 +25,5 @@ if (isset($_POST['submit'])) {
     Name: <input type="text" name="name"><br>
     Email: <input type="email" name="email"><br>
     Password: <input type="text" name="password"><br>
-    Gender:<br>
-    <input type="radio" id="laki-laki" name="gender" value="1">
-    <label for="laki-laki">Laki-laki</label>
-    <input type="radio" id="perempuan" name="gender" value="0">
-    <label for="perempuan">Perempuan</label><br>
-    Age: <input type="number" name="age"><br>
-    <button type="submit" name="submit">Save</button>
-    <button><a href="read.php">Kembali</a></button>
+  
 </form>
--- server/users/update.php
+++ server/users/update.php
@@ -20,16 +20,5 @@ if (isset($_POST['update'])) {
     Name: <input type="text" name="name" value="<?= $user['name']; ?>"><br>
     Email: <input type="email" name="email" value="<?= $user['email']; ?>"><br>
     Password: <input type="text" name="password" value="<?= $user['password']; ?>"><br>
-    Age: <input type="number" name="age" value="<?= $user['age']; ?>"><br>  
-     Gender: 
-    <br>
-    <input type="radio" id="laki-laki" name="gender" value="1"
-        <?= ($user['gender'] == 1 ? 'checked' : '') ?>>
-    <label for="laki-laki">Laki-laki</label>
-    <input type="radio" id="perempuan" name="gender" value="0"
-        <?= ($user['gender'] == 0 ? 'checked' : '') ?>>
-    <label for="perempuan">Perempuan</label>
-    <br>
-    <button type="submit" name="update">Update</button>
-    <button><a href="read.php">Kembali</a></button>
+    
 </form>
